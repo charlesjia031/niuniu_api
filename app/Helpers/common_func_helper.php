a01dbe09293e206c131ad03d8e4a6333d7126ef7
charles <charlesjia031@gmail.com>
2022-05-29 20:21:03 +0800

    fix

--- app/Helpers/common_func_helper.php
+++ app/Helpers/common_func_helper.php
@@ -2,11 +2,11 @@
 if (!function_exists("echoJson")) {
     function echoJson($status_code, $message, $data, $validation, $trace_code)
     {
-        $response['status_code'] = $status_code;
+        $response['statusCode'] = $status_code;
         $response['message'] = $message;
         $response['data'] = $data;
         $response['validation'] = $validation;
-        $response['trace_code'] = $trace_code;
+        $response['traceCode'] = $trace_code;
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($response);
         exit(0);
